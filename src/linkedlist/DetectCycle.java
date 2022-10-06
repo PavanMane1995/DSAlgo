8d7dc13fa6ccb66b1faead641567d4b9bdbaa587
PavanMane1995 <letscode1995@gmail.com>
2022-10-07 02:04:58 +0530

    detect cycle in linked list

---
+++ src/linkedlist/DetectCycle.java
@@ -0,0 +1,34 @@
+package linkedlist;
+
+public class DetectCycle {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = head.next;
+
+        System.out.println(detectCycle(head).val);
+
+
+    }
+
+    public static ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        if (fast == null || fast.next == null) return null;
+        while (head != slow) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
+    }
+
+
+}
