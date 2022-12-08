8fc2495c1aaee14a23c4ed572f2d4f2e555a838c
PavanMane1995 <letscode1995@gmail.com>
2022-12-09 00:12:34 +0530

    longest substring count

---
+++ src/array/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,31 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+//https://www.youtube.com/watch?v=wiGpQwVHdE0
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public static void main(String[] args) {
+        String a = "abcabcdbbcdfrg";
+        System.out.println(lengthOfLongestSubstring(a));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        int c=0, i=0;
+        HashSet<Character> set = new HashSet<>();
+        int max =0;
+        while(i < s.length()){
+            if(!set.contains(s.charAt(i))){
+                set.add(s.charAt(i++));
+                max = Math.max(max, set.size());
+            }else{
+                set.remove(s.charAt(c++));
+            }
+        }
+
+        return max;
+    }
+}
